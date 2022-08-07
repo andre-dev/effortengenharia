10e22ddd974cd97fc94be165c035249809ed1732
andre-dev <dark9113@hotmail.com>
2022-08-07 14:45:57 -0300

    Ajuste rodape.php

--- mapos-effort/application/views/tema/rodape.php
+++ mapos-effort/application/views/tema/rodape.php
@@ -1,6 +1,6 @@
 <div class="row-fluid">
     <div id="footer" class="span12"> <a class="pecolor" href="https://github.com/RamonSilva20/mapos" target="_blank">
-            <?= date('Y'); ?> &copy; Ramon Silva - Map-OS - Versão:
+            <?= date('Y'); ?> &copy; Todos os direitos reservados a Effort Engenharia - Versão:
       <?= $this->config->item('app_version'); ?></a></div>
 </div>
 <!--end-Footer-part-->

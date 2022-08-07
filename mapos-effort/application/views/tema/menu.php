8c4998cc05bae9b78962fe40b6be90cc5cd5cdeb
andre-dev <dark9113@hotmail.com>
2022-08-07 14:55:38 -0300

    Exclusão do icone ao lado esquedo no logo

--- mapos-effort/application/views/tema/menu.php
+++ mapos-effort/application/views/tema/menu.php
@@ -1,9 +1,9 @@
 <!--sidebar-menu-->
 <nav id="sidebar">
     <div id="newlog">
-        <div class="icon2">
+        <!-- <div class="icon2">
             <img src="<?php echo base_url() ?>assets/img/logo-two.png">
-        </div>
+        </div> -->
         <div class="title1">
             <?= $configuration['app_theme'] == 'white' ? '<img src="' . base_url() . 'assets/img/effortLogo.png">' : '<img src="' . base_url() . 'assets/img/effortLogo.png">'; ?>
         </div>

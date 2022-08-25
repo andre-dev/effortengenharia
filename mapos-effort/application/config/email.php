62ea1178b692b7fa9f7e9ba2af5e50e18d419cff
andre-dev <dark9113@hotmail.com>
2022-08-25 09:54:14 -0300

    ajuste email

--- mapos-effort/application/config/email.php
+++ mapos-effort/application/config/email.php
@@ -2,14 +2,14 @@
 
 $config['protocol']         = 'smtp';
 $config['smtp_host']        = 'smtp.hostinger.com';
-$config['smtp_crypto']      = 'tls'; // tls or ssl
-$config['smtp_port']        = 465;
+$config['smtp_crypto']      = 'ssl'; // tls or ssl
+$config['smtp_port']        = 587;
 $config['smtp_user']        = 'comercial@effortengenharia.com';
 $config['smtp_pass']        = '!Ironmah2';
-$config['validate']         = false; // validar email
-$config['mailtype']         = 'text'; // text ou html
+$config['validate']         = true; // validar email
+$config['mailtype']         = 'html'; // text ou html
 $config['charset']          = 'utf-8';
 $config['newline']          = "\r\n";
 $config['bcc_batch_mode']   = false;
 $config['wordwrap']         = false;
-$config['priority']         = 3; // 1, 2, 3, 4, 5 | Email Priority. 1 = highest. 5 = lowest. 3 = normal.
+$config['priority']         = 1; // 1, 2, 3, 4, 5 | Email Priority. 1 = highest. 5 = lowest. 3 = normal.
